b9e1248d62e699c926240c8caa938c92b07a5873
mizoru <mizoru@WIN-NGGGUVS0PCV.localdomain>
2022-10-03 18:54:42 +0300

    add deepspeech2

---
+++ hw_asr/model/DeepSpeech2.py
@@ -0,0 +1,45 @@
+from torch import nn
+from torch.nn import Sequential
+
+from hw_asr.base import BaseModel
+
+
+class RNNWrapper(nn.Module):
+  def __init__(self, *args, **kwargs):
+    super().__init__()
+    self.net = nn.RNN(*args, **kwargs)
+
+  def forward(self, x):
+    x, _ = self.net(x)
+    return x
+
+class DeepSpeech2(BaseModel):
+  def __init__(self, n_feats=128, rnn_hidden=1024, n_class=28, n_rnn=6):
+    super().__init__(n_feats, n_class)
+    self.cnn = Sequential(
+        nn.Conv2d(1, 32, (41, 11), (2,2), padding = (20, 5)), 
+        nn.ReLU(),
+        nn.Conv2d(32, 32, (21,11), (2,1), padding = (0, 5)),
+        nn.ReLU(),
+        nn.Conv2d(32, 96, (21, 11), (2,1), padding = (0, 5)),
+        nn.ReLU()
+        )
+    rnn_layers = [RNNWrapper(96, rnn_hidden, bidirectional=True, batch_first=True, nonlinearity='relu'), nn.LazyBatchNorm1d()]
+    for i in range(n_rnn-1):
+      rnn_layers.append(RNNWrapper(rnn_hidden*2, rnn_hidden, bidirectional=True, batch_first=True, nonlinearity='relu'))
+      rnn_layers.append(nn.LazyBatchNorm1d())
+
+    self.rnn = Sequential(*rnn_layers)
+    self.linear = nn.Linear(rnn_hidden*2, n_class)
+
+  def forward(self, spectrogram, **batch):
+    x = spectrogram.unsqueeze(1)
+    x = self.cnn(x)
+    x = x.squeeze(2)
+    x = x.transpose(1, 2)
+    x = self.rnn(x)
+    x = self.linear(x)
+    return {"logits": x}
+  
+  def transform_input_lengths(self, input_lengths):
+    return (input_lengths - 1)//2 + 1
--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,7 @@
 from hw_asr.model.baseline_model import BaselineModel
+from hw_asr.model.DeepSpeech2 import DeepSpeech2
 
 __all__ = [
     "BaselineModel",
+    "DeepSpeech2"
 ]
