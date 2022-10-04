5f5c87ba1784d3323cde94a0d65b37aac7f0b57b
mizoru <mizoru@WIN-NGGGUVS0PCV.localdomain>
2022-10-04 16:42:31 +0300

    logging first_batch final

--- hw_asr/trainer/trainer.py
+++ hw_asr/trainer/trainer.py
@@ -48,7 +48,8 @@ class Trainer(BaseTrainer):
             # iteration-based training
             self.train_dataloader = inf_loop(self.train_dataloader)
             self.len_epoch = len_epoch
-        self.evaluation_dataloaders = {k: v for k, v in dataloaders.items() if k != "train"}
+        self.evaluation_dataloaders = {
+            k: v for k, v in dataloaders.items() if k != "train"}
         self.lr_scheduler = lr_scheduler
         self.log_step = 50
 
@@ -71,7 +72,8 @@ class Trainer(BaseTrainer):
     def _clip_grad_norm(self):
         if self.config["trainer"].get("grad_norm_clip", None) is not None:
             clip_grad_norm_(
-                self.model.parameters(), self.config["trainer"]["grad_norm_clip"]
+                self.model.parameters(
+                ), self.config["trainer"]["grad_norm_clip"]
             )
 
     def _train_epoch(self, epoch):
@@ -90,12 +92,7 @@ class Trainer(BaseTrainer):
             # log first batch
             log_batch = batch_idx == 0 and epoch == self.start_epoch
             if log_batch:
-                self.logger.info("\nFirst input batch:")
-                for (key, value) in batch.items():
-                    if "length" not in key and isinstance(value, torch.Tensor):
-                        self.logger.info(f"\t{key}.shape: {value.shape}")
-                    else:
-                        self.logger.info(f"\t{key}: {value}")
+                self._log_batch(batch, message="\nFirst input batch:")
             try:
                 batch = self.process_batch(
                     batch,
@@ -103,16 +100,13 @@ class Trainer(BaseTrainer):
                     metrics=self.train_metrics,
                 )
                 if log_batch:
-                    self.logger.info("First output batch:")
-                    for key_to_log in ("logits", "log_probs", "log_probs_length"):
-                        if key_to_log in batch:
-                            if "length" not in key and isinstance(value, torch.Tensor):
-                                self.logger.info(f"\t{key}.shape: {value.shape}")
-                            else:
-                                self.logger.info(f"\t{key}: {value}")
+                    msg = "First output batch:"
+                    keys_to_log = ("logits", "log_probs", "log_probs_length")
+                    self._log_batch(batch, msg, keys_to_log)
             except RuntimeError as e:
                 if "out of memory" in str(e) and self.skip_oom:
-                    self.logger.warning("OOM on batch. Skipping batch.\n", exc_info)
+                    self.logger.warning(
+                        "OOM on batch. Skipping batch.\n", exc_info)
                     for p in self.model.parameters():
                         if p.grad is not None:
                             del p.grad  # free some memory
@@ -144,7 +138,8 @@ class Trainer(BaseTrainer):
 
         for part, dataloader in self.evaluation_dataloaders.items():
             val_log = self._evaluation_epoch(epoch, part, dataloader)
-            log.update(**{f"{part}_{name}": value for name, value in val_log.items()})
+            log.update(**{f"{part}_{name}": value for name,
+                       value in val_log.items()})
 
         return log
 
@@ -233,8 +228,10 @@ class Trainer(BaseTrainer):
             inds[: int(ind_len)]
             for inds, ind_len in zip(argmax_inds, log_probs_length.numpy())
         ]
-        argmax_texts_raw = [self.text_encoder.decode(inds) for inds in argmax_inds]
-        argmax_texts = [self.text_encoder.ctc_decode(inds) for inds in argmax_inds]
+        argmax_texts_raw = [self.text_encoder.decode(
+            inds) for inds in argmax_inds]
+        argmax_texts = [self.text_encoder.ctc_decode(
+            inds) for inds in argmax_inds]
         tuples = list(zip(argmax_texts, text, argmax_texts_raw, audio_path))
         shuffle(tuples)
         rows = {}
@@ -250,7 +247,8 @@ class Trainer(BaseTrainer):
                 "wer": wer,
                 "cer": cer,
             }
-        self.writer.add_table("predictions", pd.DataFrame.from_dict(rows, orient="index"))
+        self.writer.add_table(
+            "predictions", pd.DataFrame.from_dict(rows, orient="index"))
 
     def _log_spectrogram(self, spectrogram_batch):
         spectrogram = random.choice(spectrogram_batch.cpu())
@@ -265,7 +263,8 @@ class Trainer(BaseTrainer):
         parameters = [p for p in parameters if p.grad is not None]
         total_norm = torch.norm(
             torch.stack(
-                [torch.norm(p.grad.detach(), norm_type).cpu() for p in parameters]
+                [torch.norm(p.grad.detach(), norm_type).cpu()
+                 for p in parameters]
             ),
             norm_type,
         )
@@ -275,4 +274,14 @@ class Trainer(BaseTrainer):
         if self.writer is None:
             return
         for metric_name in metric_tracker.keys():
-            self.writer.add_scalar(f"{metric_name}", metric_tracker.avg(metric_name))
+            self.writer.add_scalar(
+                f"{metric_name}", metric_tracker.avg(metric_name))
+
+    def _log_batch(self, batch,  message="Batch:", keys_to_log=None):
+        self.logger.info(message)
+        for (key, value) in batch.items():
+            if not keys_to_log or key in keys_to_log:
+                if "length" not in key and isinstance(value, torch.Tensor):
+                    self.logger.info(f"\t{key}.shape: {value.shape}")
+                else:
+                    self.logger.info(f"\t{key}: {value}")
