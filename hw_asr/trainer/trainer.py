ed589032678e2ca1774dc65438c9570456535f41
mizoru <svmaxim2001@gmail.com>
2022-10-14 18:53:56 +0300

    .

--- hw_asr/trainer/trainer.py
+++ hw_asr/trainer/trainer.py
@@ -131,6 +131,7 @@ class Trainer(BaseTrainer):
                 )
                 self._log_predictions(**batch)
                 self._log_spectrogram(batch["spectrogram"])
+                self._log_audio(**batch)
                 self._log_scalars(self.train_metrics)
                 # we don't want to reset train metrics at the start of every epoch
                 # because we are interested in recent train metrics
