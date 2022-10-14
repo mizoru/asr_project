0e3b0e67289eeab39a65ecb0230906fa519b8cc9
mizoru <svmaxim2001@gmail.com>
2022-10-14 20:14:40 +0300

    augments

--- hw_asr/augmentations/wave_augmentations/ColoredNoise.py
+++ hw_asr/augmentations/wave_augmentations/ColoredNoise.py
@@ -4,7 +4,7 @@ from torch import Tensor
 from hw_asr.augmentations.base import AugmentationBase
 
 
-class PitchShift(AugmentationBase):
+class ColoredNoise(AugmentationBase):
     def __init__(self, *args, **kwargs):
         self._aug = torch_audiomentations.AddColoredNoise(*args, **kwargs)
 
--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,11 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.ColoredNoise import ColoredNoise
+from hw_asr.augmentations.wave_augmentations.PitchShift import PitchShift
+from hw_asr.augmentations.wave_augmentations.Gain import Gain
+
 
 __all__ = [
-    "Gain"
+    "Gain",
+    "PitchShift",
+    "ColoredNoise"
 ]
