8d7327628a6d9217f2e8a984b24d0366b7a05c75
mizoru <svmaxim2001@gmail.com>
2022-10-16 21:36:28 +0300

    fix

--- hw_asr/metric/lm_metric.py
+++ hw_asr/metric/lm_metric.py
@@ -16,7 +16,7 @@ class LMDecoderWERMetric(BaseMetric):
     def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs):
         wers = []
 
-        predictions = [self.ctc_decode_lm(
+        predictions = [self.text_encoder.ctc_decode_lm(
             log_probs[i], log_probs_length[i]) for i in range(log_probs.size(0))]
 
         for pred_text, target_text in zip(predictions, text):
